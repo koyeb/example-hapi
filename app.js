51fa06d2698fed0da0328e2598cacb95755f9d86
Justin Ellingwood <justin.ellingwood@gmail.com>
2024-03-05 11:10:31 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// app.js
+
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+const port = process.env.PORT || 3000;
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: port,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World!';
+        }
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
